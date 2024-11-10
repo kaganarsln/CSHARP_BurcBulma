9bd126a3c9dde496b42a9ac3c7e6e638d7bed8af
kaganarsln <kaan.arslangorurr@gmail.com>
2024-11-10 19:31:44 +0300

    CSHARP_BurcBulma
    C# WİNDOWS FORMS İLE GELİŞTİRİLMİŞTİR. Doğum gününe kaç gün kaldığını, bugüne dek kaç gün yaşadığınızı ve burcunuzu hesaplamak için geliştirilmiştir

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace VİZE3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Doğum Tarihi:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Burcunuz:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Bugüne Kadar Yaşadığınız Gün";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(217, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Doğum Gününüze Kalan Gün Sayısı";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Burcunuzun Özellikleri";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(295, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(295, 127);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(295, 169);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(295, 208);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(200, 230);
+            this.textBox4.TabIndex = 9;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(295, 46);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(520, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(268, 269);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(589, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 41);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace VİZE3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int yasananZaman;
+
+            DateTime dogumTarihiGir = dateTimePicker1.Value;
+            DateTime bugun = DateTime.Now;
+
+            int farkYil = bugun.Year - dogumTarihiGir.Year;
+            int farkAy = bugun.Month - dogumTarihiGir.Month;
+            int farkGun = bugun.Day - dogumTarihiGir.Day;
+
+            yasananZaman = (bugun - dogumTarihiGir).Days;
+            textBox2.Text = yasananZaman.ToString() + " gün" + " " + "(" + farkYil + " yıl/" + farkAy + " ay/" + farkGun + " gün)";
+
+            DateTime sonrakiDogumGunu;
+            if (bugun.Month < dogumTarihiGir.Month || bugun.Month == dogumTarihiGir.Month && bugun.Day < dogumTarihiGir.Day)
+            {
+                sonrakiDogumGunu = new DateTime(bugun.Year, dogumTarihiGir.Month, dogumTarihiGir.Day);
+            }
+            else
+            {
+                sonrakiDogumGunu = new DateTime(bugun.Year + 1, dogumTarihiGir.Month, dogumTarihiGir.Day);
+            }
+
+            int kalanGun = (sonrakiDogumGunu - bugun).Days;
+            textBox3.Text = kalanGun.ToString() + " gün";
+
+            if (dogumTarihiGir.Month == 1 && dogumTarihiGir.Day >= 21 || dogumTarihiGir.Month == 2 && dogumTarihiGir.Day <= 19)
+            {
+                textBox1.Text = "Kova";
+                textBox4.Text = "Arkadaş ve dost canlısı, sosyal, yenilikçi, sıradışı, marjinal";
+                pictureBox1.ImageLocation = "C:\\Users\\user\\Desktop\\gorselprogramlama1\\VİZE3\\burç fotoğraflar\\basak.jpg   ";
+            }
+            else if (dogumTarihiGir.Month == 2 && dogumTarihiGir.Day >= 20 || dogumTarihiGir.Month == 3 && dogumTarihiGir.Day <= 20)
+            {
+                textBox1.Text = "Balık";
+                textBox4.Text = "Hassas, duyarlı, empatik, vicdanlı, duyarlı, sanatsal, hayırsever, yardımsever, kolay etkilenen";
+                pictureBox1.ImageLocation = "C:\\Users\\user\\Desktop\\gorselprogramlama1\\VİZE3\\burç fotoğraflar\\balik.jpg";
+            }
+            else if (dogumTarihiGir.Month == 3 && dogumTarihiGir.Day >= 21 || dogumTarihiGir.Month == 4 && dogumTarihiGir.Day <= 20)
+            {
+                textBox1.Text = "Koç";
+                textBox4.Text = "Aktif, dinamik, insiyatif sahibi, hızlı, lider, yönetici";
+                pictureBox1.ImageLocation = "C:\\C:\\Users\\user\\Desktop\\gorselprogramlama1\\VİZE3\\burç fotoğraflar\\koc.jpg";
+            }
+            else if (dogumTarihiGir.Month == 4 && dogumTarihiGir.Day >= 21 || dogumTarihiGir.Month == 5 && dogumTarihiGir.Day <= 20)
+            {
+                textBox1.Text = "Boğa";
+                textBox4.Text = "Güvenilir, rahatına, konforuna, yemeğe düşkün, gurme, harekete geçmekte zorlanan";
+                pictureBox1.ImageLocation = "C:\\Users\\user\\Desktop\\gorselprogramlama1\\VİZE3\\burç fotoğraflar\\boga.jpg";
+            }
+            else if (dogumTarihiGir.Month == 5 && dogumTarihiGir.Day >= 21 || dogumTarihiGir.Month == 6 && dogumTarihiGir.Day <= 21)
+            {
+                textBox1.Text = "İkizler";
+                textBox4.Text = "Esnek, uyumlu, çevik, konuşkan, meraklı, arkadaş canlısı";
+                pictureBox1.ImageLocation = "C:\\Users\\user\\Desktop\\gorselprogramlama1\\VİZE3\\burç fotoğraflar\\ikizler.jpg";
+            }
+            else if (dogumTarihiGir.Month == 6 && dogumTarihiGir.Day >= 22 || dogumTarihiGir.Month == 7 && dogumTarihiGir.Day <= 22)
+            {
+                textBox1.Text = "Yengeç";
+                textBox4.Text = "Evine, annesine, ailesine, yurduna düşkün, duyarlı, hassas, yemeğe, sofraya meraklı, tarihsel konulara ilgili, müzik yeteneğine sahip";
+                pictureBox1.ImageLocation = "C:\\Users\\user\\Desktop\\gorselprogramlama1\\VİZE3\\burç fotoğraflar\\yengec.jpg";
+            }
+            else if (dogumTarihiGir.Month == 7 && dogumTarihiGir.Day >= 23 || dogumTarihiGir.Month == 8 && dogumTarihiGir.Day <= 23)
+            {
+                textBox1.Text = "Aslan";
+                textBox4.Text = "Özgüvenli, gururlu, mağrur, koruyan, kollayan, gösterişli, sanata, estetiğe düşkün";
+                pictureBox1.ImageLocation = "C:\\Users\\user\\Desktop\\gorselprogramlama1\\VİZE3\\burç fotoğraflar\\aslan.jpg";
+            }
+            else if (dogumTarihiGir.Month == 8 && dogumTarihiGir.Day >= 24 || dogumTarihiGir.Month == 9 && dogumTarihiGir.Day <= 23)
+            {
+                textBox1.Text = "Başak";
+                textBox4.Text = "Titiz, çalışkan, mükemmeliyetçi, eleştirel, detaycı, emektar, hizmet odaklı";
+                pictureBox1.ImageLocation = "C:\\Users\\user\\Desktop\\gorselprogramlama1\\VİZE3\\burç fotoğraflar\\basak.jpg";
+            }
+            else if (dogumTarihiGir.Month == 9 && dogumTarihiGir.Day >= 24 || dogumTarihiGir.Month == 10 && dogumTarihiGir.Day <= 23)
+            {
+                textBox1.Text = "Terazi";
+                textBox4.Text = "Adil, eşitlikçi, dengeli, uyumlu, sakin, sabırlı, akıllı, kibar";
+                pictureBox1.ImageLocation = "C:\\Users\\user\\Desktop\\gorselprogramlama1\\VİZE3\\burç fotoğraflar\\terazi.jpg";
+            }
+            else if (dogumTarihiGir.Month == 10 && dogumTarihiGir.Day >= 24 || dogumTarihiGir.Month == 11 && dogumTarihiGir.Day <= 22)
+            {
+                textBox1.Text = "Akrep";
+                textBox4.Text = "Mücadeleci, güçlü, yılmaz, hırslı kişilerdir. Pes ettiğini düşündüğünüz anda küllerinden yeniden doğabilir. Güvenilir, kaliteli insanlardır.";
+                pictureBox1.ImageLocation = "C:\\Users\\user\\Desktop\\gorselprogramlama1\\VİZE3\\burç fotoğraflar\\akrep.jpg";
+            }
+            else if (dogumTarihiGir.Month == 11 && dogumTarihiGir.Day >= 23 || dogumTarihiGir.Month == 12 && dogumTarihiGir.Day <= 21)
+            {
+                textBox1.Text = "Yay";
+                textBox4.Text = "Bağımsız, özgür, hareketli, dinamik, sportif, keşif ve macera ruhuna sahip";
+                pictureBox1.ImageLocation = "C:\\Users\\user\\Desktop\\gorselprogramlama1\\VİZE3\\burç fotoğraflar\\yay.jpg";
+            }
+            else if (dogumTarihiGir.Month == 12 && dogumTarihiGir.Day >= 22 || dogumTarihiGir.Month == 1 && dogumTarihiGir.Day <= 20)
+            {
+                textBox1.Text = "Oğlak";
+                textBox4.Text = "Ciddi, soğukkanlı, gereğinde mesafeli, planlı, organize, disiplinli, iş ve kariyer odaklı";
+                pictureBox1.ImageLocation = "C:\\Users\\user\\Desktop\\gorselprogramlama1\\VİZE3\\burç fotoğraflar\\oglak.jpg";
+            }
+        }
+    }
+}
